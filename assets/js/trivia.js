fb4fbb101947fe7b649cf4d83ad2b97fd37a0450
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-04-29 10:05:31 -0700

    fixed score and question number

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,11 +1,13 @@
-var modalBtn = document.querySelector('.modal-btn');
-var modalBg = document.querySelector('.modal-bg');
-var modalClose = document.querySelector('.modal-close');
-var startTrivia = document.querySelector('#btnStartTrivia');
-var questionObjs = []
-var currentQuestion = 0;
-var score = 0;
-var missed = 0;
+let modalBtn = document.querySelector('.modal-btn');
+let modalBg = document.querySelector('.modal-bg');
+let modalClose = document.querySelector('.modal-close');
+let startTrivia = document.querySelector('#btnStartTrivia');
+let questionObjs = []
+let currentQuestion = 0;
+let score = 0;
+let missed = 0;
+let counter = 0
+let modalState = false;
 startTrivia.addEventListener('click', function () {
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
 
@@ -13,18 +15,30 @@ startTrivia.addEventListener('click', function () {
         .then(data => {
             questionObjs = data.results;
             console.log(questionObjs);
+            resetModal()
             renderQuestion()
         })
 
 })
 
-function renderQuestion() {
+function resetModal() {
+  if(!modalState) {
+    currentQuestion = 0;
+    score = 0;
+    missed = 0;
+    counter = 0;
+    console.log("RESETTING ALL THE VALUES",currentQuestion);
+    displayScore()
+  }
+}
+
+function renderQuestion() { 
     console.log(questionObjs[currentQuestion])
     document.querySelector(".question").innerHTML = questionObjs[currentQuestion].question
-    var ind = Math.floor(Math.random() * 3.2) // random number 0-3
-    var choices = ["!", "!", "!", "!"]
-    var j = 0
-    choices[ind] = questionObjs[currentQuestion].correct_answer;
+    let randomNumber = Math.floor(Math.random() * 3.2) // random number 0-3
+    let choices = ["!", "!", "!", "!"]
+    let j = 0
+    choices[randomNumber] = questionObjs[currentQuestion].correct_answer;
     for (let i = 0; i < 4; i++) {
         if (choices[i] === "!") {
             choices[i] = questionObjs[currentQuestion].incorrect_answers[j]
@@ -45,7 +59,11 @@ modalBtn.addEventListener('click', function () {
     modalBg.classList.add('bg-active');
 });
 modalClose.addEventListener('click', function () {
+    console.log("the modal fired thank you")
     modalBg.classList.remove('bg-active');
+    modalState = false;
+    console.log("SETTING MODAL STATE TO FALSE ")
+
 })
 
 document.getElementById("a").addEventListener("click", checkAnswer)
@@ -54,26 +72,44 @@ document.getElementById("c").addEventListener("click", checkAnswer)
 document.getElementById("d").addEventListener("click", checkAnswer)
 
 function checkAnswer() {
-    var currentSelection = this.getAttribute("data-value")
-    console.log(this, currentSelection)
-    if (currentSelection == questionObjs[currentQuestion].correct_answer) {
-        score++
-    } else {
-        missed++
-    }
-    if (currentQuestion < questionObjs.length - 1) {
-        currentQuestion++;
-        renderQuestion()
-    } else {
-        displayScore()
+    let currentSelection = this.getAttribute("data-value")
+    console.log(this, questionObjs.length)
+    if(currentQuestion <= questionObjs.length - 1) {
+      console.log(`THE CURRENT QUESTION IS ${currentQuestion}`)
+      if (currentSelection == questionObjs[currentQuestion].correct_answer ) {
+          if(counter < 5) {
+            score++
+            counter++
+            displayScore()
+            console.log("THIS IS THE COUNTER",counter)
+            
+          }
+      } else {
+          if(counter < 5) {
+            missed++
+            counter++
+            displayScore()
+            console.log("THIS IS THE COUNTER",counter)
+          }
+      }
+      
+      if (currentQuestion < questionObjs.length - 1) {
+          ++currentQuestion;
+          renderQuestion()
+      } else {
+        // console.log("what is this condition")
+          // displayScore()
 
+      }
     }
 }
 
 
 function displayScore() {
-    modalBg.classList.remove('bg-active');
+    // modalBg.classList.remove('bg-active');
     document.getElementById("score").innerHTML = `<h1>Score:${score}</h1> <p>Missed: ${missed}</p>`
+    document.getElementById("question-number").innerHTML = `<h1>Question:${counter < 4 ? counter + 1: 5}/5</h1>`
+
 }
 
 
